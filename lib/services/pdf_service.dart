386f4d8fe9fbbe5762668b55c4491b91c82ad655
mustapha amin <mustaphaamin2003@gmail.com>
2023-07-24 21:37:18 +0100

    added try catch

--- lib/services/pdf_service.dart
+++ lib/services/pdf_service.dart
@@ -1,21 +1,30 @@
 import 'dart:developer';
 import 'dart:io';
+import 'package:cloud_vault/views/widgets/error_dialog.dart';
 import 'package:dio/dio.dart';
 import 'package:firebase_storage/firebase_storage.dart';
+import 'package:flutter/cupertino.dart';
 import 'package:flutter/foundation.dart';
 import 'package:flutter/services.dart';
 import 'package:open_file_plus/open_file_plus.dart';
 import 'package:path_provider/path_provider.dart';
 
 class PDFService {
-  static Future<String?> loadPDF(String? url) async {
-    final ref = FirebaseStorage.instance.ref(url);
+  static Future<String?> loadDocument(BuildContext context, String? url) async {
     try {
       final fileName = _stripAuthParamsFromURL(url!.split('/').last);
       final dir = await getApplicationDocumentsDirectory();
       final file = File('${dir.path}/$fileName');
-      await Dio().download(url, file.path);
-      return file.path;
+      if (await file.exists()) {
+        return file.path;
+      }
+      try {
+        await Dio().download(url, file.path);
+        return file.path;
+      } catch (e) {
+        showErrorDialog(context, e.toString());
+        return null;
+      }
     } catch (e) {
       return e.toString();
     }
